6302829436266dfbb6fd0f4ee26ba96364d7d2b1
Adepoju Oluwatobi <46296263+adepoju-oluwatobi@users.noreply.github.com>
2023-08-23 07:22:47 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ import AdminRegistration from "./pages/AdminRegistration";
 
 const router = createBrowserRouter([
   {
-    path: "/home",
+    path: "/honeylandcooperativeportal",
     element: <App />,
   },
   {
